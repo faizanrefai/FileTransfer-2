ca151fff981675b25ea3fbbac9e446814f651c9e
hoan.ha <hoan.ha@htklabs.com>
2012-10-28 10:27:34 +0700

    Initial Commit

---
+++ FileTransfer/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FileTransfer
+//
+//  Created by Admin on 10/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
